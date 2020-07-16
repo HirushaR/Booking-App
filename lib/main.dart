f088efcaf79a81bb111b0a17ba99f8b5ccd54c4b
HirushaR <hirusharandunu11@gmail.com>
2020-07-16 17:59:05 +0530

    create stack

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() =>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Stack(
+              children: <Widget>[
+//                new Container(),
+               new Container(
+                height: 60.0,
+                width: 60.0,
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.circular(50.0),
+                  color: Colors.green
+                ),
+                 child: new Icon(Icons.local_offer,color: Colors.white),
+               ),
+                new Container(
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFFFC6A7F),
+                  ),
+                  child: new Icon(Icons.local_offer,color: Colors.white),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
