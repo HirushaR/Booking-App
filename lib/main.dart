dcbf5221547bfdea07d4dfea2e3e414e3aa5cf08
HirushaR <hirusharandunu11@gmail.com>
2020-07-16 18:12:08 +0530

    add main title

--- lib/main.dart
+++ lib/main.dart
@@ -22,18 +22,19 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Stack(
+              alignment: Alignment.center,
               children: <Widget>[
-//                new Container(),
                new Container(
                 height: 60.0,
                 width: 60.0,
                 decoration: new BoxDecoration(
                   borderRadius: new BorderRadius.circular(50.0),
-                  color: Colors.green
+                  color: Color(0xFF18D191)
                 ),
                  child: new Icon(Icons.local_offer,color: Colors.white),
                ),
                 new Container(
+                  margin: new EdgeInsets.only(right: 50.0,top: 50.0),
                   height: 60.0,
                   width: 60.0,
                   decoration: new BoxDecoration(
@@ -41,6 +42,38 @@ class HomePage extends StatelessWidget {
                       color: Color(0xFFFC6A7F),
                   ),
                   child: new Icon(Icons.home,color: Colors.white),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(left: 30.0,top: 60.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.circular(50.0),
+                    color: Color(0xFFFFCE56),
+                  ),
+                  child: new Icon(Icons.local_car_wash,color: Colors.white),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(left: 80.0,top: 0.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.circular(50.0),
+                    color: Color(0xFF45E0EC),
+                  ),
+                  child: new Icon(Icons.place,color: Colors.white),
+                ),
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: new Text(
+                    "Quick Bee",
+                    style: TextStyle(fontSize: 30.0),
+                  ),
                 ),
               ],
             )
