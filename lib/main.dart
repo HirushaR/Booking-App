989a9d8a55d354f82e3d1cb7e5398e3aadb934c0
HirushaR <hirusharandunu11@gmail.com>
2020-07-16 18:29:09 +0530

    create the loginng buttons

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            //ui design
             new Stack(
               alignment: Alignment.center,
               children: <Widget>[
@@ -65,11 +66,12 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
+            //Main text
             new Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.only(top:8.0,bottom: 80.0),
                   child: new Text(
                     "Quick Bee",
                     style: TextStyle(fontSize: 30.0),
@@ -77,11 +79,12 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
+            //SIgn in with email
             new Row(
               children: <Widget>[
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.only(left:20.0,right: 20.0,top: 10.0),
                     child: new Container(
                       alignment: Alignment.center,
                       height: 60.0,
@@ -97,7 +100,47 @@ class HomePage extends StatelessWidget {
                   ),
                 )
               ],
-            )
+            ),
+            //Sign in with Facebook
+            new Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left:20.0,right: 10.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(
+                          color: Color(0xFF4363A1),
+                          borderRadius: new BorderRadius.circular(10.0)
+                      ),
+                      child:  Text("Facebook",
+                        style: TextStyle(fontSize: 20.0,color: Colors.white),
+
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left:10.0,right: 20.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(
+                          color: Color(0xFFDF513B),
+                          borderRadius: new BorderRadius.circular(10.0)
+                      ),
+                      child:  Text("Google",
+                        style: TextStyle(fontSize: 20.0,color: Colors.white),
+
+                      ),
+                    ),
+                  ),
+                )
+
+              ],
+            ),
           ],
         ),
       ),
